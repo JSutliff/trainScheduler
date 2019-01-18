f017838a194ad11b63f5709c6e7dbfea9b22756a
JSutliff <sutliff.jason@gmail.com>
2019-01-18 13:14:00 -0800

    general site layout complete / firebase added to js file

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello World');
+var config = {
+  apiKey: "AIzaSyAXjE76F9CAv-0BJdZ8qy_fXIgK2zcb0yk",
+  authDomain: "trainscheduler-45e9f.firebaseapp.com",
+  databaseURL: "https://trainscheduler-45e9f.firebaseio.com",
+  projectId: "trainscheduler-45e9f",
+  storageBucket: "",
+  messagingSenderId: "797441266955"
+};
+
+firebase.initializeApp(config);
+
+var db = firebase.database();
+
